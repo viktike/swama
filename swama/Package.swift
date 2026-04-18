9106b39666eec64f1516d3d998dfdd8dea65a190
Sperl Viktor <viktike32@gmail.com>
2026-04-18 21:49:04 +0200

    Update dependecies.

--- swama/Package.swift
+++ swama/Package.swift
@@ -19,9 +19,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
-        .package(url: "https://github.com/viktike/mlx-swift-lm", branch: "viktike"),
-        .package(url: "https://github.com/ml-explore/mlx-swift", branch: "main"),
-        .package(url: "https://github.com/viktike/mlx-swift-audio.git", branch: "main"),
+        .package(url: "https://github.com/viktike/vmlx-swift-lm.git", branch: "new"),
+        .package(url: "https://github.com/osaurus-ai/mlx-swift", branch: "osaurus-0.31.3"),
+        .package(url: "https://github.com/viktike/mlx-swift-audio.git", branch: "new"),
     ],
     targets: [
         .target(
@@ -29,10 +29,10 @@ let package = Package(
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
-                .product(name: "MLXLLM", package: "mlx-swift-lm"),
-                .product(name: "MLXVLM", package: "mlx-swift-lm"),
-                .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
-                .product(name: "MLXEmbedders", package: "mlx-swift-lm"),
+                .product(name: "MLXLLM", package: "vmlx-swift-lm"),
+                .product(name: "MLXVLM", package: "vmlx-swift-lm"),
+                .product(name: "MLXLMCommon", package: "vmlx-swift-lm"),
+                .product(name: "MLXEmbedders", package: "vmlx-swift-lm"),
                 .product(name: "MLXRandom", package: "mlx-swift"),
                 .product(name: "MLXFast", package: "mlx-swift"),
                 .product(name: "MLXAudio", package: "mlx-swift-audio"),
